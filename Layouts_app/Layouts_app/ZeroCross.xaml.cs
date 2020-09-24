41f01cfef2ebfac78c8cbaff0101962c8008661d
Faskin <alex2002tyn@gmail.com>
2020-09-24 10:48:58 +0300

    Update
    New background color, reset button, correct grid display

--- Layouts_app/Layouts_app/ZeroCross.xaml.cs
+++ Layouts_app/Layouts_app/ZeroCross.xaml.cs
@@ -12,18 +12,19 @@ namespace Layouts_app
 {
     public partial class ZeroCross : ContentPage
     {
+        bool turn;
+        Label cT;
         Image boxs;
         Button resetButton;
         const int gridColumns = 3;
 
         public ZeroCross()
         {
-
-            StackLayout stackLayout = new StackLayout();
+            
             Grid ground = new Grid()
             {
-                HeightRequest = 100,
-            }
+                HeightRequest = 600,
+            };
             for (int i = 0; i < gridColumns; i++)
             {
                 ground.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
@@ -35,7 +36,7 @@ namespace Layouts_app
                 for (int j = 0; j < gridColumns; j++)
                 {
                     boxs = new Image
-                    {HeightRequest = 100, BackgroundColor = Color.FromHex("#2485AB")};
+                    {HeightRequest = 100, BackgroundColor = Color.FromHex("DEF7FE") };
                     ground.Children.Add(boxs, i, j);
                     var tap = new TapGestureRecognizer();
                     tap.Tapped += Tap_Tapped;
@@ -44,13 +45,52 @@ namespace Layouts_app
             }
             resetButton = new Button()
             {
-                Text = "Reset Game"
+                Text = "Reset Game",
+                BackgroundColor = Color.FromHex("DEF7FE"),
+                HorizontalOptions = LayoutOptions.StartAndExpand
+
+            };
+            cT = new Label()
+            {
+                TextColor = Color.White,
+                HorizontalOptions = LayoutOptions.EndAndExpand,
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+
+            };
+            
+           
+            StackLayout buttonLayout = new StackLayout()
+            {
+                BackgroundColor = Color.Black,
+                Children = { resetButton }
+            };
+            StackLayout stackLayout = new StackLayout()
+            {
+                BackgroundColor = Color.Black,
+                Children = { ground, buttonLayout, cT }
+            };
+
+            Content = stackLayout;
+            cT.Text = turn ? "X Player" : "Z Player";
+            
+        }
+        private void updateT()
+        {
+            if (turn = true)
+            {
+                turn = false;
+                cT.Text = "X Player";
+
+            }
+            if (turn = false)
+            {
+                turn = true;
+                cT.Text = "Z Player";
             }
         }
-        
         private void Tap_Tapped(object sender, EventArgs e)
         {
-
+            
             
         }
     }
