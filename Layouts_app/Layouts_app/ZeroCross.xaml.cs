2ddfb20e0ca2ea583b72ea632ac48ef1c6844d3c
Faskin <alex2002tyn@gmail.com>
2020-09-28 23:06:07 +0300

    tapped reset button

--- Layouts_app/Layouts_app/ZeroCross.xaml.cs
+++ Layouts_app/Layouts_app/ZeroCross.xaml.cs
@@ -7,12 +7,14 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Markup;
 
 namespace Layouts_app
 {
     public partial class ZeroCross : ContentPage
     {
-        Grid ground;
+        
         bool turn;
         Label cT;
         Image boxs;
@@ -21,17 +23,17 @@ namespace Layouts_app
         private const bool Zero = false;
         private const bool Cross = true;
         Dictionary<Image, int> ticTac;
-        Dictionary<Image, int[]> boxsPos;
         public ZeroCross()
         {
-            
+            ticTac = new Dictionary<Image, int>();
             Grid ground = new Grid()
             {
-                HeightRequest = 600,
+                HeightRequest = 500,
             };
             for (int i = 0; i < gridColumns; i++)
             {
                 ground.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                ground.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
             }
             
             for (int i = 0; i < gridColumns; i++)
@@ -39,19 +41,21 @@ namespace Layouts_app
                 for (int j = 0; j < gridColumns; j++)
                 {
                     boxs = new Image
-                    {HeightRequest = 100, BackgroundColor = Color.FromHex("DEF7FE") };
+                    {HeightRequest = 100, BackgroundColor = Color.FromHex("DEF7FE")
+                    };
+
                     ground.Children.Add(boxs, i, j);
-                    boxsPos.Add(boxs, new int[2] { i, j });
                     var tap = new TapGestureRecognizer();
                     boxs.GestureRecognizers.Add(tap);
-                    tap.Tapped += box_Tapped;
+                    tap.Tapped += Box_Tapped;
                 }
             }
             resetButton = new Button()
             {
                 Text = "Reset Game",
                 BackgroundColor = Color.FromHex("DEF7FE"),
-                HorizontalOptions = LayoutOptions.StartAndExpand
+                HorizontalOptions = LayoutOptions.StartAndExpand,
+                
 
             };
             cT = new Label()
@@ -76,10 +80,18 @@ namespace Layouts_app
 
             Content = stackLayout;
             randomTurn();
-            
+            resetButton.Clicked += ResetButton_Clicked;
+
+
         }
 
-        private void box_Tapped(object sender, EventArgs e)
+        private void ResetButton_Clicked(object sender, EventArgs e)
+        {
+            boxs.Source = "nothing.png";
+            updateT(boxs);
+        }
+
+        private void Box_Tapped(object sender, EventArgs e)
         {
             Image boxs = sender as Image;
             if (turn == Cross && !ticTac.ContainsKey(boxs))
@@ -88,13 +100,17 @@ namespace Layouts_app
                 updateT(boxs);
 
             }
+
             else if (turn == Zero && !ticTac.ContainsKey(boxs))
             {
                 ticTac[boxs] = 2;
                 updateT(boxs);
             }
+            
+
 
         }
+
 
         private void randomTurn()
         {
@@ -106,16 +122,17 @@ namespace Layouts_app
         {
             if (ticTac[boxs] == 1)
             {
-                boxs.Source = "cross.jpg";
+                boxs.Source = "cross.png";
                 turn = Cross;
-                cT.Text = "This turn: 1 Player";
+                cT.Text = "1 Player";
 
             }
-            else
+
+            else 
             {
-                boxs.Source = "zero.jpg";
+                boxs.Source = "zero.png";
                 turn = Zero;
-                cT.Text = "This turn: 2 Player";
+                cT.Text = "2 Player";
             }
             
         }
